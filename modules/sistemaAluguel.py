8e3856a440b0f063b2f9ca875bcee7d9e4643c37
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-10-20 18:29:08 -0300

    update: Sistema de aluguel

---
+++ modules/sistemaAluguel.py
@@ -0,0 +1,13 @@
+class SistemaDeAluguel:
+    def __init__(self):
+        self.frota = []
+
+    def adicionar_veiculo(self, veiculo):
+        self.frota.append(veiculo)
+
+    def mostrar_frota(self):
+        for veiculo in self.frota:
+            veiculo.mostrar_info()
+
+    def calcular_total_veiculos(self):
+        return len(self.frota)
