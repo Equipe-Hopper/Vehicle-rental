242b95067ab76d96e3c60aea02d71c7caf146810
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-10-20 18:24:08 -0300

    Update moto.py

--- modules/moto.py
+++ modules/moto.py
@@ -1,11 +1,22 @@
 from modules.veiculos import Veiculo
 
 class Moto(Veiculo):
-    def __init__(self, modelo, marca, ano_veiculo, valor_aluguel_dia, combustivel,cilindrada):
-        super().__init__(modelo, marca, ano_veiculo, valor_aluguel_dia, combustivel,cilindrada)
-    
-    def calcular_aluguel(self, dias, desconto=0,cilindrada=None):
-        if cilindrada>200:
-            pass
-            
-        return super().calcular_aluguel(dias, desconto)
+    def __init__(self, modelo, marca, ano_veiculo, valor_aluguel_dia, combustivel, cilindrada) -> None:
+        super().__init__(modelo, marca, ano_veiculo, valor_aluguel_dia, combustivel)
+        self.__cilindrada = cilindrada
+
+    @property
+    def cilindrada(self):
+        return self.__cilindrada
+
+    @cilindrada.setter
+    def cilindrada(self, cilindrada):
+        self.__cilindrada = cilindrada
+
+    def calcular_aluguel(self, dias, desconto=0):
+        valor_total = super().calcular_aluguel(dias, desconto)
+
+        if self.__cilindrada > 200:
+            valor_total *= 1.15
+
+        return valor_total
